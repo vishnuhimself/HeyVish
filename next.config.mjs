8215b73c7dd07a50398fc8cb3fa0c0710aa254bb
Srivishnu Ramakrishnan <dreams.sri@gmail.com>
2025-01-03 17:58:38 +0530

    Setup redirects for WP Slugs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,80 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async redirects() {
+    return [
+      {
+        source: '/wordpress-shortcode-to-get-estimated-reading-time',
+        destination: '/blog/wordpress-shortcode-to-get-estimated-reading-time',
+        permanent: true,
+      },
+      {
+        source: '/wordpress-remove-url-field-in-comments-section',
+        destination: '/blog/wordpress-remove-url-field-in-comments-section',
+        permanent: true,
+      },
+      {
+        source: '/wordpress-show-featured-image-after-first-paragraph',
+        destination: '/blog/wordpress-show-featured-image-after-first-paragraph',
+        permanent: true,
+      },
+      {
+        source: '/generatepress-remove-comma-between-tags',
+        destination: '/blog/generatepress-remove-comma-between-tags',
+        permanent: true,
+      },
+      {
+        source: '/generatepress-show-category-below-title',
+        destination: '/blog/generatepress-show-category-below-title',
+        permanent: true,
+      },
+      {
+        source: '/disable-self-pingbacks-on-wordpress',
+        destination: '/blog/disable-self-pingbacks-on-wordpress',
+        permanent: true,
+      },
+      {
+        source: '/add-php-wordpress',
+        destination: '/blog/add-php-wordpress',
+        permanent: true,
+      },
+      {
+        source: '/total-number-of-image-in-a-site-shortcode',
+        destination: '/blog/total-number-of-image-in-a-site-shortcode',
+        permanent: true,
+      },
+      {
+        source: '/total-number-of-categories-shortcode',
+        destination: '/blog/total-number-of-categories-shortcode',
+        permanent: true,
+      },
+      {
+        source: '/total-posts-shortcode',
+        destination: '/blog/total-posts-shortcode',
+        permanent: true,
+      },
+      {
+        source: '/disable-comments-wordpress',
+        destination: '/blog/disable-comments-wordpress',
+        permanent: true,
+      },
+      {
+        source: '/post-modified-date-shortcode',
+        destination: '/blog/post-modified-date-shortcode',
+        permanent: true,
+      },
+      {
+        source: '/set-the-first-image-as-featured-image',
+        destination: '/blog/set-the-first-image-as-featured-image',
+        permanent: true,
+      },
+      {
+        source: '/total-images-in-a-post-shortcode',
+        destination: '/blog/total-images-in-a-post-shortcode',
+        permanent: true,
+      },
+    ]
+  },
 }
  
 const withMDX = createMDX({
